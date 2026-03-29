e9f2f750a829b145936a1160e31e03eb795694e0
Mr-Azam-AHS <eazam@andrews.esc18.net>
2026-03-28 20:11:08 -0500

    Typo fix

--- src/main/java/WordSort.java
+++ src/main/java/WordSort.java
@@ -3,37 +3,32 @@
 //Name - Avery
 
 import java.util.Arrays;
-import static java.lang.System.*; 
+import static java.lang.System.*;
 
-public class WordSort
-{
+public class WordSort {
 	private String[] wordRay;
 
-	public WordSort(String line)
-	{
-	   setList(line);
+	public WordSort(String line) {
+		setList(line);
 	}
 
-	public void setList(String line)
-	{
+	public void setList(String line) {
 		wordRay = line.split(" ");
 		System.out.print(Arrays.toString(wordRay));
 	}
 
-	public void sort()
-	{
-		wordRay = Arrays.sort(wordRay);
+	public void sort() {
+		Arrays.sort(wordRay);
 		System.out.print(Arrays.toString(wordRay));
 	}
 
-	public String toString( )
-	{
+	public String toString() {
 		Arrays.sort(wordRay);
 		String list = "";
 		System.out.println();
-		for(int i = 0; i < wordRay.length; i++){
+		for (int i = 0; i < wordRay.length; i++) {
 			list += "word " + i + " :: " + wordRay[i] + "\n";
 		}
-		return list;
+		return list + "\n\n";
 	}
 }
